5f7babdbef38ba7a98bbde72ad0544227d5c8a90
Ludo van den Boom <ludo@cubicphuse.nl>
2011-07-28 22:13:51 +0200

    Exercise

---
+++ lib/elderly_discount_calculator.rb
@@ -0,0 +1,34 @@
+# Calculator for elderly discount
+class ElderlyDiscountCalculator
+  ELDERLY_DISCOUNT = 739
+  ELDERLY_DISCOUNT_SINGLE = 421
+
+  ELDERLY_DISCOUNT_MAXIMUM_INCOME = 34857
+
+  # Constructor
+  #
+  # @param [Fixnum] income Persn's current income
+  # @param [Fixnum] age Current age
+  # @param [Boolean] single True when person has no partner
+  def initialize(income, age, single)
+    @income = income
+    @age = age
+    @single = single
+  end
+
+  # Perform calculation on input to determine the elderly discount
+  #
+  # @return [Fixnum] Discount
+  def discount
+    # TODO Exercise 1: Implement this method by making tests pass, try to incrementally implement the method. Choose one test and write just enough code to make it pass, then move on to the next test.
+    raise NotImplementedError
+  end
+
+  # Find out if a person is eligible for elderly discount, based on input
+  #
+  # @return [Boolean] True when person eligible for elderly discount
+  def eligible?
+    # TODO Exercise 2: Create implementation for this method in a TDD-way
+    raise NotImplementedError
+  end
+end
---
+++ spec/elderly_discount_calculator_spec.rb
@@ -0,0 +1,108 @@
+require 'elderly_discount_calculator'
+
+describe ElderlyDiscountCalculator do
+  describe "#calculate" do
+    before(:each) do
+      @attrs = {}
+    end
+
+    def calculator(attrs={})
+      attrs = { income: 36000, age: 65, single: true }.merge(attrs)
+      ElderlyDiscountCalculator.new(attrs[:income], attrs[:age], attrs[:single])
+    end
+
+    describe "when person 65 or older" do
+      before(:each) do
+        @attrs[:age] = 65
+      end
+
+      describe "and single" do
+        before(:each) do
+          @attrs[:single] = true
+        end
+
+        describe "and earns less than maximum income" do
+          before(:each) do
+            @attrs[:income] = ElderlyDiscountCalculator::ELDERLY_DISCOUNT_MAXIMUM_INCOME - 1
+          end
+
+          it "should receive elderly discount for single person" do
+            calculator(@attrs).discount.should == ElderlyDiscountCalculator::ELDERLY_DISCOUNT_SINGLE
+          end
+        end
+
+        describe "and earns exactly the maximum income" do
+          before(:each) do
+            @attrs[:income] = ElderlyDiscountCalculator::ELDERLY_DISCOUNT_MAXIMUM_INCOME
+          end
+
+          it "should receive elderly discount for single person" do
+            calculator(@attrs).discount.should == ElderlyDiscountCalculator::ELDERLY_DISCOUNT_SINGLE
+          end
+        end
+
+        describe "and earns more than maximum income" do
+          before(:each) do
+            @attrs[:income] = ElderlyDiscountCalculator::ELDERLY_DISCOUNT_MAXIMUM_INCOME + 1
+          end
+
+          it "should not receive elderly discount" do
+            calculator(@attrs).discount.should == 0
+          end
+        end
+      end
+
+      describe "and not single" do
+        before(:each) do
+          @attrs[:single] = false
+        end
+
+        describe "and earns less than maximum income" do
+          before(:each) do
+            @attrs[:income] = ElderlyDiscountCalculator::ELDERLY_DISCOUNT_MAXIMUM_INCOME - 1
+          end
+
+          it "should receive elderly discount" do
+            calculator(@attrs).discount.should == ElderlyDiscountCalculator::ELDERLY_DISCOUNT
+          end
+        end
+
+        describe "and earns exactly the maximum income" do
+          before(:each) do
+            @attrs[:income] = ElderlyDiscountCalculator::ELDERLY_DISCOUNT_MAXIMUM_INCOME
+          end
+
+          it "should receive elderly discount" do
+            calculator(@attrs).discount.should == ElderlyDiscountCalculator::ELDERLY_DISCOUNT
+          end
+        end
+
+        describe "and earns more than maximum income" do
+          before(:each) do
+            @attrs[:income] = ElderlyDiscountCalculator::ELDERLY_DISCOUNT_MAXIMUM_INCOME + 1
+          end
+
+          it "should not receive elderly discount" do
+            calculator(@attrs).discount.should == 0
+          end
+        end
+      end
+    end
+
+    describe "when person younger than 65" do
+      before(:each) do
+        @attrs[:age] = 64
+        @attrs[:income] = ElderlyDiscountCalculator::ELDERLY_DISCOUNT_MAXIMUM_INCOME - 1
+        @attrs[:single] = false
+      end
+
+      it "should not receive elderly discount" do
+        calculator(@attrs).discount.should be_zero
+      end
+    end
+  end
+
+  describe "#eligible?" do
+    it "should have specs"
+  end
+end

3fd4a37dc9f80abf36961202b6dd82becde347b9
Patrick Baselier <patrick.baselier@gmail.com>
2011-09-23 10:47:12 +0200

    Fixed failing specs.

--- lib/elderly_discount_calculator.rb
+++ lib/elderly_discount_calculator.rb
@@ -21,7 +21,14 @@ class ElderlyDiscountCalculator
   # @return [Fixnum] Discount
   def discount
     # TODO Exercise 1: Implement this method by making tests pass, try to incrementally implement the method. Choose one test and write just enough code to make it pass, then move on to the next test.
-    raise NotImplementedError
+    if @age < 65
+      return 0
+    end
+    if @single
+      @income <= ELDERLY_DISCOUNT_MAXIMUM_INCOME ? ELDERLY_DISCOUNT_SINGLE : 0 
+    else
+      @income <= ELDERLY_DISCOUNT_MAXIMUM_INCOME ? ELDERLY_DISCOUNT : 0 
+    end 
   end
 
   # Find out if a person is eligible for elderly discount, based on input
